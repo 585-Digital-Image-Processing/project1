9542b7ab66c19203fbd1e589a54a51efab133439
jiuchao <jiuchao1@gmail.com>
2020-01-30 20:08:42 -0500

    Update bitand_s.m

--- bitand_s.m
+++ bitand_s.m
@@ -1,3 +1,27 @@
+%%%%%%%%%%%%%  Function erosion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Purpose:  
+%      Compute the bit-and operation of two input matrices
+%      
+% Input Variables:
+%      A       MxN input 2D binary image 
+%      B       MxN input 2D binary image
+%      
+% Returned Results:
+%      C       result after bit-and operation of A and B
+%
+% Processing Flow:
+%      1. Loop through every element in A and B. Check whether elements at
+%      the same position (x,y) in A and B are the same.
+%           If yes, then in the result image C, C(x,y) = A(x,y)
+%           Otherwise, C(x,y) = 0
+%
+%  Restrictions/Notes:
+%      The two input image should be of the same dimension 
+%
+%  The following functions are called:
+%      none
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function C = bitand_s(A, B)
 
