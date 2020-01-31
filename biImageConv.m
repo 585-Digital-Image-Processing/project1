50aca8aacb2b30197b7c9bbfc1f52f578ff9f542
JessicaYangyx <1622004399@qq.com>
2020-01-30 23:09:03 -0500

    update the note in biImageConv

--- biImageConv.m
+++ biImageConv.m
@@ -1,3 +1,28 @@
+%%%%%%%%%%%%%  Function biImageConv %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Purpose:  
+%      Convert the grey image to a binary image
+%      
+% Input Variables:
+%      A       MxN input 2D grey image 
+%      
+% Returned Results:
+%      B       MxN output 2D binary image
+%
+% Processing Flow:
+%      1. Set the threshold to divide each pixel into 0 or 1. 
+%      2. If A(x,y)<threshold, then in the result image B, B(x,y) = 1,which 
+%         represents the foreground (i.e. the circles).
+%         Otherwise, B(x,y) = 0, which represents the background.
+%
+%  Restrictions/Notes:
+%      The input image should be 2D grey image 
+%
+%  The following functions are called:
+%      none
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function B = biImageConv(A)
 
 % Set the threshold of gray level
