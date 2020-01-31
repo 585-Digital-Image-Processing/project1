3f363b7ddea97ef588e836e583ca7176603d47a4
jiuchao <jiuchao1@gmail.com>
2020-01-30 21:25:46 -0500

    Update main.m

--- main.m
+++ main.m
@@ -1,11 +1,10 @@
 %%%%%%%%%%%%% main.m file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Purpose:  
-%      Realize Hit-or-miss  
+%      Realize Hit-or-miss 
 %
 % Input Variables:
-%      f            input 2D rgb image
-%
-%      
+%      f            input 2D 8-bit image
+%    
 % Returned Results:
 %      B            A binary image of f
 %      new_B        Image B after noise removal
@@ -13,20 +12,40 @@
 %      biggest      An image indicating locations of biggest circles in B
 %
 % Processing Flow:
-%      1.  Load, extract the gray layer, and display the input image
-%      2.  Convert the input image into binary image
-%      3.  use opening and closing to remove noise at the background and
-%          inside circles
-%      4.  Find all circles in the image and get their centers and radius
+%      1. Load, extract the gray layer, and display the input image
+%      2. Convert the input image into binary image
+%      3. use opening and closing to remove noise at the background and
+%         inside circles
+%      4. Find all circles in the image and get their centers and radius,
+%         especially the radius of the smallest and biggest circles: 
+%         min_r and max_r.
+%      5. Find the locations of smallest circles:
+%         - Erode the binary image with a structure element: a circle with 
+%           radius (min_r) - 1.
+%         - Erode the complement of the image with a structure element: a
+%           square having a round cavity with radius (min_r) + 1. 
+%         - Perform bitand of the two resulted matrices
+%         - The circles with radius in range [(min_r) - 1, (min_r) + 1] will 
+%           be located.
+%      6. Find the locations of biggest circles:
+%         - Erode the binary image with a structure element: a circle with 
+%           radius (max_r) - 1.
+%         - Erode the complement of the image with a structure element: a
+%           square having a round cavity with radius (max_r) + 1. 
+%         - Perform bitand of the two resulted matrices
+%         - The circles with radius in range [(max_r) - 1, (max_r) + 1] will 
+%           be located.
 %          
 %
 %  Restrictions/Notes:
 %      This function requires an 8-bit image as input.  
 %
 %  The following functions are called:
-%      zero.m       create an image full of zeroes
-%      mean3x3.m    filter with a 3x3 mean filter
-%          worked into the code below - try to do this yourself
+%      bilImageConv.m       Convert an 8-bit image to a binary image
+%      opening.m            Remove the noise outside circles
+%      closing.m            Fill the noise in the circles
+%      erosion.m            Erode an image with a structure element
+%      bitand_s.m           Perform bitand operation of two matrices
 %
 %  Author:      Yanxi Yang, Jiuchao Yin, Hongjie Liu
 %  Date:        1/30/2020
